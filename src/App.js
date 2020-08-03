aca9486be324c905c3170f9048b0d6a45507874d
Florian Soller <flsoller@gmail.com>
2020-08-03 22:28:19 +0200

    Add function for updating books state.

--- src/App.js
+++ src/App.js
@@ -20,11 +20,11 @@ function App() {
         <MenuBar />
       </Grid>
       <Grid item container>
-        <Grid item xs={0} sm={2} />
+        <Grid item xs={false} sm={2} />
         <Grid item xs={12} sm={8}>
           <ContentHolder />
         </Grid>
-        <Grid item xs={0} sm={2} />
+        <Grid item xs={false} sm={2} />
       </Grid>
     </Grid>
   );
--- src/components/content/ContentHolder.jsx
+++ src/components/content/ContentHolder.jsx
@@ -3,7 +3,7 @@
 import React, { useState } from 'react';
 
 import CardItem from './CardItem';
-import { Grid, makeStyles } from '@material-ui/core';
+import { Grid, makeStyles, Button } from '@material-ui/core';
 
 // Material-UI styling hook.
 const useStyles = makeStyles({
@@ -19,20 +19,36 @@ function ContentHolder() {
 
   // React useState hook.
   const [books, setBooks] = useState([
-    { title: 'BookTitle', pages: 300 },
-    { title: 'BookTitle', pages: 300 },
-    { title: 'BookTitle', pages: 300 },
-    { title: 'BookTitle', pages: 300 },
+    { title: 'BookTitle', pages: 300, key: Math.random() },
+    { title: 'BookTitle', pages: 300, key: Math.random() },
+    { title: 'BookTitle', pages: 300, key: Math.random() },
+    { title: 'BookTitle', pages: 300, key: Math.random() },
   ]);
+
+  // Preparation for updating state. TODO: Make dynamic.
+  const addBook = () => {
+    setBooks([
+      ...books,
+      {
+        title: 'NewBook',
+        pages: 400,
+        key: Math.random(),
+      },
+    ]);
+  };
+
+  // Display all books from state.
+  const renderBooks = books.map((book) => (
+    <Grid item xs={12} sm={6} md={4} key={book.key}>
+      <CardItem />
+    </Grid>
+  ));
 
   //JSX
   return (
     <Grid container spacing={2} className={classes.topMargin}>
-      {books.map((book) => (
-        <Grid item xs={12} sm={6} md={4}>
-          <CardItem />
-        </Grid>
-      ))}
+      {renderBooks}
+      <Button onClick={addBook}>Add Book</Button>
     </Grid>
   );
 }
