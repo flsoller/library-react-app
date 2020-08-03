6497408cc7a0e0781e423371f2a9c9e533ec8e71
Florian Soller <flsoller@gmail.com>
2020-08-03 21:44:03 +0200

    Add state hook to ContentHolder.

--- src/components/content/ContentHolder.jsx
+++ src/components/content/ContentHolder.jsx
@@ -1,6 +1,6 @@
 // Maintains content. Currently placeholder data / hard coded
 
-import React from 'react';
+import React, { useState } from 'react';
 
 import CardItem from './CardItem';
 import { Grid, makeStyles } from '@material-ui/core';
@@ -14,13 +14,25 @@ const useStyles = makeStyles({
 
 //Component
 function ContentHolder() {
+  // Material UI styles
   const classes = useStyles();
+
+  // React useState hook.
+  const [books, setBooks] = useState([
+    { title: 'BookTitle', pages: 300 },
+    { title: 'BookTitle', pages: 300 },
+    { title: 'BookTitle', pages: 300 },
+    { title: 'BookTitle', pages: 300 },
+  ]);
+
   //JSX
   return (
     <Grid container spacing={2} className={classes.topMargin}>
-      <Grid item xs={12} sm={6} md={4}>
-        <CardItem />
-      </Grid>
+      {books.map((book) => (
+        <Grid item xs={12} sm={6} md={4}>
+          <CardItem />
+        </Grid>
+      ))}
     </Grid>
   );
 }
