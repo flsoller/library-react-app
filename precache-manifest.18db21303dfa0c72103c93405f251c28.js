c47b6abecb375e8c1e31c705d6303f5e81745d74
Florian Soller <flsoller@gmail.com>
2020-08-15 20:18:04 +0200

    Updates

---
+++ precache-manifest.18db21303dfa0c72103c93405f251c28.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2762b8938a96b82910af5de915f5f4a1",
+    "url": "/library-react-app/index.html"
+  },
+  {
+    "revision": "f963997476ffe01c3b73",
+    "url": "/library-react-app/static/css/main.33e0e534.chunk.css"
+  },
+  {
+    "revision": "34ef2f2e4478893e5654",
+    "url": "/library-react-app/static/js/2.a2a6075b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/library-react-app/static/js/2.a2a6075b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f963997476ffe01c3b73",
+    "url": "/library-react-app/static/js/main.1f2e6604.chunk.js"
+  },
+  {
+    "revision": "e64152a37c7d6a42a496",
+    "url": "/library-react-app/static/js/runtime-main.4db0124c.js"
+  },
+  {
+    "revision": "8ac9c07935dd277e1374ebf404ea64b4",
+    "url": "/library-react-app/static/media/defaultBackground.8ac9c079.svg"
+  }
+]);
