ec3a2f9844e776f6dac2f4139aab283a0d5c2eed
Florian Soller <flsoller@gmail.com>
2020-10-28 20:50:45 +0100

    Define constants.

---
+++ src/redux/constants.js
@@ -0,0 +1,8 @@
+// Library related constants
+export const LIBRARY_ADD_BOOK = 'LIBRARY_ADD_BOOK';
+
+// Filtered view related constants
+export const VIEW_SELECT_ALL = 'VIEW_SELECT_ALL';
+export const VIEW_SELECT_FAV = 'VIEW_SELECT_FAV';
+export const VIEW_SELECT_READ = 'VIEW_SELECT_READ';
+export const VIEW_SELECT_LOANED = 'VIEW_SELECT_LOANED';
