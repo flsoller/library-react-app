8d0fe96321f80bd80e640854b97186705e8fad53
Florian Soller <flsoller@gmail.com>
2020-10-31 18:06:25 +0100

    Enable redux devtools extension.

--- src/redux/store.js
+++ src/redux/store.js
@@ -11,7 +11,10 @@ const reducer = combineReducers({
 });
 
 // Create store
-const store = createStore(reducer);
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 console.log(store.getState());
 
 export default store;
