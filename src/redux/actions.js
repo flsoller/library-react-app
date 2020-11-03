cd2988f1923182e54e08599075f677a59ef415f4
Florian Soller <flsoller@gmail.com>
2020-11-03 20:23:55 +0100

    Add view actions.

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,8 +1,35 @@
-import { LIBRARY_ADD_BOOK } from './constants';
+import {
+  LIBRARY_ADD_BOOK,
+  VIEW_SELECT_ALL,
+  VIEW_SELECT_LOANED,
+  VIEW_SELECT_READ,
+  VIEW_SELECT_FAV,
+} from './constants';
 
 export const addToLibrary = (book) => {
   return {
     type: LIBRARY_ADD_BOOK,
     payload: book,
+  };
+};
+
+export const viewAllBooks = () => {
+  return {
+    type: VIEW_SELECT_ALL,
+  };
+};
+export const viewLoanedBooks = () => {
+  return {
+    type: VIEW_SELECT_LOANED,
+  };
+};
+export const viewReadBooks = () => {
+  return {
+    type: VIEW_SELECT_READ,
+  };
+};
+export const viewFavBooks = () => {
+  return {
+    type: VIEW_SELECT_FAV,
   };
 };
