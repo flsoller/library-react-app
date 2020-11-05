5b53700d4c2c7adb6ed9abaab40a076b92b98d0f
Florian Soller <flsoller@gmail.com>
2020-11-05 18:33:46 +0100

    Add constant.

--- src/redux/constants.js
+++ src/redux/constants.js
@@ -1,5 +1,6 @@
 // Library related constants
 export const LIBRARY_ADD_BOOK = 'LIBRARY_ADD_BOOK';
+export const LIBRARY_REMOVE_BOOK = 'LIBRARY_REMOVE_BOOK';
 
 // Filtered view related constants
 export const VIEW_SELECT_ALL = 'VIEW_SELECT_ALL';
