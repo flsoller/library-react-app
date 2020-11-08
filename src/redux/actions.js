33cfedbad4e7f7e0688d441b336fdfc62abdb24e
Florian Soller <flsoller@gmail.com>
2020-11-08 12:21:52 +0100

    Remove localStorage function.
    LocalStorage was implemented incorrect. Handled thorugh store subscribe.

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -10,8 +10,6 @@ import {
 } from './constants';
 
 export const addToLibrary = (book) => {
-  localStorage.setItem('books', JSON.stringify(store.getState().library));
-
   return {
     type: LIBRARY_ADD_BOOK,
     payload: book,
