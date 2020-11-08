990b0897a9829fedf480b87d7c546f72dfb912b8
Florian Soller <flsoller@gmail.com>
2020-11-08 12:18:11 +0100

    localStorage subscribes to library state changes.

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,6 +3,7 @@ import { createStore, combineReducers } from 'redux';
 
 // File imports
 import { libraryReducer, viewReducer } from './reducers';
+import { saveToLocalStorage } from './helpers/localStorage';
 
 // Create reducer
 const reducer = combineReducers({
@@ -15,5 +16,10 @@ const store = createStore(
   reducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
+// Handler for keeping localStorage in sync with library state
+store.subscribe(() => {
+  saveToLocalStorage(store.getState().library);
+});
 
 export default store;
